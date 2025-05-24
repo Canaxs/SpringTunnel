b5b7135ab26c77d660264c9f1e7d2698fcf820ac
Meriç Cana <mmericcana@gmail.com>
2025-05-24 16:55:30 +0300

    Initial project

---
+++ src/main/java/com/springtunnel/SpringtunnelApplication.java
@@ -0,0 +1,13 @@
+package com.springtunnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtunnelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtunnelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springtunnel/SpringtunnelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtunnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtunnelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
